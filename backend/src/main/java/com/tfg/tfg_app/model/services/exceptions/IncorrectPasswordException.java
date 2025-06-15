65451ae057e0e757a85b118304b3563a805e130b
--globally <p.mendez.vazquez@udc.es>
2025-06-15 03:05:09 +0200

    incorrect password exception

---
+++ backend/src/main/java/com/tfg/tfg_app/model/services/exceptions/IncorrectPasswordException.java
@@ -0,0 +1,7 @@
+package com.tfg.tfg_app.model.services.exceptions;
+
+/**
+ * The Class IncorrectPasswordException.
+ */
+@SuppressWarnings("serial")
+public class IncorrectPasswordException extends Exception {}
