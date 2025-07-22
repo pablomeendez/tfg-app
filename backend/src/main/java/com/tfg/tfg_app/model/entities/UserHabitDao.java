213b33121e7ba414ce3641bc6ba691746717499a
--globally <p.mendez.vazquez@udc.es>
2025-07-22 02:06:34 +0200

    UserHabit Dao

--- backend/src/main/java/com/tfg/tfg_app/model/entities/UserHabitDao.java
+++ backend/src/main/java/com/tfg/tfg_app/model/entities/UserHabitDao.java
@@ -1,6 +1,10 @@
 package com.tfg.tfg_app.model.entities;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserHabitDao extends JpaRepository<UserHabit, Long> {
+
+    List<UserHabit> findByUserId(Long userId);
 } 
