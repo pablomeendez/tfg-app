bded20f7cfb52ad76074dcfe368e6310be3250fb
--globally <p.mendez.vazquez@udc.es>
2025-07-23 04:33:23 +0200

    HabitEntryDao new function

--- backend/src/main/java/com/tfg/tfg_app/model/entities/HabitEntryDao.java
+++ backend/src/main/java/com/tfg/tfg_app/model/entities/HabitEntryDao.java
@@ -3,4 +3,6 @@ package com.tfg.tfg_app.model.entities;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HabitEntryDao extends JpaRepository<HabitEntry, Long> {
+
+    HabitEntry findTopByUserIdOrderByIdDesc(Long userId);
 } 
