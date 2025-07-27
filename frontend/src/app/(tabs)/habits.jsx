a9e04f4b9e0e6e6a5e494f9b5f4aeb8cff427231
--globally <p.mendez.vazquez@udc.es>
2025-07-27 19:23:21 +0200

    Habits Tab done

--- frontend/src/app/(tabs)/habits.jsx
+++ frontend/src/app/(tabs)/habits.jsx
@@ -1,9 +1,118 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import habitService from '../../services/habitService';
+import HabitCard from '../../components/HabitCard';
 
 export default function Habits() { 
+    const [allHabits, setAllHabits] = useState([]);
+    const [myHabits, setMyHabits] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchHabits = async () => {
+            try {
+                const response = await habitService.getAllHabits();
+                const userId = await AsyncStorage.getItem('userId');
+                const myHabitsResponse = await habitService.getHabitsByUser(userId);
+                setAllHabits(response.data);
+                setMyHabits(myHabitsResponse.data); 
+                setLoading(false);
+            } catch (err) {
+                console.error('Error fetching habits:', err);
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+
+        fetchHabits();
+    }, []);
+
+    const handleAddHabit = async (habitId) => {
+        try {
+            await habitService.createUserHabit(habitId);
+            const userId = await AsyncStorage.getItem('userId');
+            const updatedMyHabits = await habitService.getHabitsByUser(userId);
+            
+            setMyHabits(updatedMyHabits.data);
+        } catch (err) {
+            console.error('Error adding habit:', err);
+            setError(err.message);
+        }
+    }
+
+    const handleDeleteHabit = async (userHabitId) => {
+        try {
+            await habitService.deleteUserHabit(userHabitId);
+            const userId = await AsyncStorage.getItem('userId');
+            const updatedMyHabits = await habitService.getHabitsByUser(userId);
+            
+            setMyHabits(updatedMyHabits.data);
+        } catch (err) {
+            console.error('Error deleting habit:', err);
+            setError(err.message);
+        }
+    }
+
+    if (loading) {
+        return (
+            <View className="flex-1 justify-center items-center">
+                <ActivityIndicator />
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View className="flex-1 justify-center items-center">
+                <Text>Error: {error}</Text>
+            </View>
+        );
+    }
+
     return (
-        <View className="flex-1 justify-center items-center bg-black">
-            <Text className="text-white">Habits</Text>
-        </View>
-    )
+        <ScrollView className="flex-1 p-4">
+            <View className="mb-6">
+                <Text className="text-2xl font-bold text-gray-800 mb-4">
+                    Mis Hábitos
+                </Text>
+                {myHabits.map((userHabit, index) => (
+                    <View key={`my-habit-${index}`}>
+                        <HabitCard habit={allHabits.find(h => h.id === userHabit.habit.id)} />
+                        <View className="flex-row justify-center items-center mt-2">
+                            <TouchableOpacity 
+                                className="bg-red-500 w-9/12 justify-center h-10 rounded-lg" 
+                                onPress={() => {
+                                    handleDeleteHabit(userHabit.id)
+                                }}>
+                                <Text className="text-center text-white font-semibold">Remove</Text> 
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                ))}
+            </View>
+
+            <View>
+                <Text className="text-2xl font-bold text-gray-800 mb-4">
+                    Todos los Hábitos
+                </Text>
+                {allHabits.map((habit, index) => (
+                    <View key={`my-habit-${index}`}>
+                        <HabitCard habit={habit} />
+                        {myHabits.some(myHabit => myHabit.habit.id === habit.id) ? null : (
+                            <View className="flex-row justify-center items-center mt-2">
+                            <TouchableOpacity 
+                                className="bg-blue-500 w-9/12 justify-center h-10 rounded-lg" 
+                                onPress={() => handleAddHabit(habit.id)}
+                                >
+                                <Text className="text-center text-white font-semibold">Add</Text>
+                            </TouchableOpacity>
+                        </View>
+                        ) }
+                    </View>
+                ))}
+            </View>
+        </ScrollView>
+    );
 }
