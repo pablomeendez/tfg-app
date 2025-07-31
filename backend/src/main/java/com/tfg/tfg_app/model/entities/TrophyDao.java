2ca58d2bab518ad25536b4ddedb87938ab43c6b9
--globally <p.mendez.vazquez@udc.es>
2025-07-31 02:48:38 +0200

    Trophy Dao updates

--- backend/src/main/java/com/tfg/tfg_app/model/entities/TrophyDao.java
+++ backend/src/main/java/com/tfg/tfg_app/model/entities/TrophyDao.java
@@ -3,4 +3,6 @@ package com.tfg.tfg_app.model.entities;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrophyDao extends JpaRepository<Trophy, Long> {
+    
+    Trophy findFirstByDays(int days);
 } 
--- backend/src/main/java/com/tfg/tfg_app/model/entities/UserTrophyDao.java
+++ backend/src/main/java/com/tfg/tfg_app/model/entities/UserTrophyDao.java
@@ -1,6 +1,12 @@
 package com.tfg.tfg_app.model.entities;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserTrophyDao extends JpaRepository<UserTrophy, Long> {
+
+    List<UserTrophy> findByUserId(Long userId);
+
+    List<UserTrophy> findByUserIdAndHabitId(Long userId, Long habitId);
 } 
