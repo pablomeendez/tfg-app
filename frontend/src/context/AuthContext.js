322982d3ff57097c893035cccfbf172fe9af7121
--globally <p.mendez.vazquez@udc.es>
2025-07-31 02:58:32 +0200

    AuthContext now stores the whole user

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -7,6 +7,7 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     const [userToken, setUserToken] = useState(null);
     const [userId, setUserId] = useState(null);
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const verifyUserExists = async (userId, token) => {
@@ -22,21 +23,25 @@ export const AuthProvider = ({ children }) => {
     const clearAuthData = async () => {
         await AsyncStorage.removeItem('userToken');
         await AsyncStorage.removeItem('userId');
+        await AsyncStorage.removeItem('user');
         setUserToken(null);
         setUserId(null);
-        console.log('Auth data cleared');
+        setUser(null);
     };
 
     const checkLogin = async () => {
         try {
             const token = await AsyncStorage.getItem('userToken');
             const userId = await AsyncStorage.getItem('userId');
-            if (token && userId) {
-                const userExists = await verifyUserExists(userId);
-                
+            const user = JSON.parse(await AsyncStorage.getItem('user'));
+
+            if (token && userId && user) {
+                const userExists = await verifyUserExists(userId, token);
+
                 if (userExists) {
                     setUserToken(token);
                     setUserId(userId);
+                    setUser(user);
                     console.log('User verified and logged in:', userId);
                 } else {
                     console.log('User no longer exists, clearing auth data');
@@ -61,7 +66,7 @@ export const AuthProvider = ({ children }) => {
             return await userService.register(userName, password, name, lastName, email);
         } catch (e) {
             console.log('Error in register:', e);
-            throw e;w
+            throw e;
         }
     }
 
@@ -70,11 +75,14 @@ export const AuthProvider = ({ children }) => {
             const result = await userService.login(userName, password);
             const token = result.data?.serviceToken;
             const userId = result.data?.user.id;
+            const user = result.data?.user;
             
             if (token) {
                 await AsyncStorage.setItem('userToken', token);
                 await AsyncStorage.setItem('userId', userId.toString());
+                await AsyncStorage.setItem('user', JSON.stringify(user));
                 setUserToken(token);
+                setUser(user);
                 setUserId(userId);
             } else {
                 console.log('No token received from login response'); // Debug log
@@ -91,8 +99,10 @@ export const AuthProvider = ({ children }) => {
         try {
             await userService.logout();
             await AsyncStorage.removeItem('userToken');
+            await AsyncStorage.removeItem('user');
             await AsyncStorage.removeItem('userId');
             setUserToken(null);
+            setUser(null);
             setUserId(null);
         } catch (e) {
             console.log('Error in logout:', e);
@@ -103,6 +113,7 @@ export const AuthProvider = ({ children }) => {
     const value = {
         userToken,
         userId,
+        user,
         loading,
         login,
         register,
