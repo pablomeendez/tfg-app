6424ac1463c87ff1a91931729ade0163d1d7ef2a
--globally <p.mendez.vazquez@udc.es>
2025-08-06 01:25:20 +0200

    schema change

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -62,10 +62,10 @@ CREATE TABLE IF NOT EXISTS HabitEntry (
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     streak int NOT NULL DEFAULT 0,
     userId Long NOT NULL,
-    userHabitId Long NOT NULL,
+    habitId Long NOT NULL,
     diaryEntryId Long NOT NULL,
     CONSTRAINT fk_habit_entry_user FOREIGN KEY (userId) REFERENCES Users(id),
-    CONSTRAINT fk_habit_entry_user_habit FOREIGN KEY (userHabitId) REFERENCES UserHabit(id),
+    CONSTRAINT fk_habit_entry_habit FOREIGN KEY (habitId) REFERENCES Habit(id),
     CONSTRAINT fk_habit_entry_diary_entry FOREIGN KEY (diaryEntryId) REFERENCES DiaryEntry(id)
 );
 
