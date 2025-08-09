8c9c66e89bf721c79e9fee8a8819b20c060343d8
--globally <p.mendez.vazquez@udc.es>
2025-08-09 03:04:46 +0200

    Pagination on WeeklySummaryConversor

--- backend/src/main/java/com/tfg/tfg_app/rest/dtos/WeeklySummaryConversor.java
+++ backend/src/main/java/com/tfg/tfg_app/rest/dtos/WeeklySummaryConversor.java
@@ -1,6 +1,9 @@
 package com.tfg.tfg_app.rest.dtos;
 
-import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
 import com.tfg.tfg_app.model.entities.WeeklySummary;
 
@@ -10,9 +13,9 @@ public class WeeklySummaryConversor {
         return new WeeklySummaryDto(weeklySummary.getId(), UserConversor.toUserDto(weeklySummary.getUser()), weeklySummary.getDate(), weeklySummary.getHabitsCompleted(), weeklySummary.getTotalEntries(), weeklySummary.getTrophiesEarned(), HabitConversor.toHabitEntryDto(weeklySummary.getBiggestStreak()), DiaryEntryConversor.toMoodDto(weeklySummary.getMoodTrend()));
     }
 
-    public static List<WeeklySummaryDto> toWeeklySummaryDtos(List<WeeklySummary> weeklySummaries) {
-        return weeklySummaries.stream()
+    public static Page<WeeklySummaryDto> toWeeklySummaryDtoPage(Page<WeeklySummary> weeklySummaries) {
+        return new PageImpl<>(weeklySummaries.stream()
                 .map(WeeklySummaryConversor::toWeeklySummaryDto)
-                .toList();
+                .collect(Collectors.toList()), weeklySummaries.getPageable(), weeklySummaries.getTotalElements());
     }
 }
