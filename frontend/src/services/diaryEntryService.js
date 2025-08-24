e1577f385091cf788ecab7273a4196fada230b56
--globally <p.mendez.vazquez@udc.es>
2025-08-25 01:46:42 +0200

    diaryentryservice change

--- frontend/src/services/diaryEntryService.js
+++ frontend/src/services/diaryEntryService.js
@@ -2,7 +2,7 @@ import apiClient  from "./apiClient";
 
 const diaryEntryService = {
     createDiaryEntry: (description, images, mood, selectedHabits) => {
-        return apiClient.post('/diaryEntry', {description: description, images: images, date: new Date(Date.now()), mood: mood, habits: selectedHabits});
+        return apiClient.post('/diaryEntry', {description: description, images: images, mood: mood, habits: selectedHabits});
     },
     updateDiaryEntry: (diaryEntryId, description, images, moodId) => {
         return apiClient.put(`/diaryEntry/${diaryEntryId}`, {description: description, images: images, moodId: moodId});
