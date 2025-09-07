57b7d14b99b55d6efb527f125a87680ad24e13ad
--globally <p.mendez.vazquez@udc.es>
2025-09-07 19:38:03 +0200

    JsonProcessingException deleted

--- backend/src/main/java/com/tfg/tfg_app/model/services/AssistantService.java
+++ backend/src/main/java/com/tfg/tfg_app/model/services/AssistantService.java
@@ -1,11 +1,10 @@
 package com.tfg.tfg_app.model.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.tfg.tfg_app.model.common.exceptions.InstanceNotFoundException;
 
 import dev.langchain4j.model.chat.response.ChatResponse;
 
 public interface AssistantService {
 
-    ChatResponse chat(Long userId, String question) throws InstanceNotFoundException, JsonProcessingException;
+    ChatResponse chat(Long userId, String question) throws InstanceNotFoundException;
 }
